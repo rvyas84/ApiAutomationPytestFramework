d98ddfbbeb19082a8c29515e66636c3a89528e44
rvyas84 <rajanvyas84@gmail.com>
2025-11-26 17:26:35 -0800

    nit

--- utils/api_client.py
+++ utils/api_client.py
@@ -12,8 +12,6 @@ class APIClient:
     def __init__(self):
         token = os.getenv("API_TOKEN")
 
-        print(token)
-
         if not token:
             raise ValueError("⚠ GOREST_API_TOKEN environment variable not set")
 
